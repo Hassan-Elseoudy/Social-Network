7fbae0f9b001f162bc663db56122feaef01284a7
Hassan <hassanzamalek97@gmail.com>
2019-05-19 01:32:15 +0200

    Updates

--- socialnetwork.sql
+++ socialnetwork.sql
@@ -1,10 +1,13 @@
 create database socialnetwork;
-CREATE TABLE if not exists user (
-	first_name varchar(255) not null,
-	last_name varchar(255) not null,
-	nick_name varchar(255) not null,
-	pass_word int not null,
-	user_email varchar(255) unique not null,
+use socialnetwork;
+
+CREATE TABLE user_(
+	
+	first_name varchar(255) NOT NULL,
+	last_name varchar(255) NOT NULL,
+	nick_name varchar(255),
+	pass_word varchar(255) NOT NULL,
+	user_email varchar(255) NOT NULL,
 	phone_number varchar(255),
 	home_town varchar(255) ,
 	about_me varchar(255) ,
@@ -12,16 +15,16 @@ CREATE TABLE if not exists user (
 	birth_date varchar(255),
 	user_gender varchar(255),
 	number_friends int , 
-	user_image BLOB,
-	PRIMARY KEY (user_email,nick_name)
+	user_image IMAGE,
+	PRIMARY KEY (user_email)
 );
 
-CREATE TABLE if not exists friendship (
+CREATE TABLE friendship (
 	date timestamp,
-        request_status tinyint(1),
-        user_email varchar(255),
-        friend_email varchar(255),
+    request_status tinyint(1),
+    user_email varchar(255),
+    friend_email varchar(255),
 	PRIMARY KEY (friend_email,user_email),
-	FOREIGN KEY (user_email,friend_email) REFERENCES user(user_email,user_email)
+	FOREIGN KEY (user_email,friend_email) REFERENCES user_(user_email,user_email)
 );
 
