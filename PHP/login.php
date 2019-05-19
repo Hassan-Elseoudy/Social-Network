8fa0629c5eb5d97ca77530f726894a57afa0d1ff
Hassan <hassanzamalek97@gmail.com>
2019-05-19 18:33:57 +0200

    ..

--- PHP/login.php
+++ PHP/login.php
@@ -4,6 +4,7 @@ $username = "root";
 $password = "";
 $db="socialnetwork";
 $output="";
+session_start();
 // Create connection
 $conn = new mysqli($servername, $username, $password,$db);
 
@@ -17,15 +18,26 @@ if ($conn->connect_error)
 
 		if (isset($_POST['login']))
 		{
-			
+			 
 			$password=$_POST['password'];
 			$email=$_POST['email'];
 			
-    		$q=mysqli_query($conn,"SELECT * FROM user WHERE user_email='$email' and pass_word='$password'");
+    		$q=mysqli_query($conn,"SELECT * FROM user_ WHERE user_email='$email' and pass_word='$password'");
     		$row=mysqli_fetch_array($q);
     		if($row['user_email']==$email&&$row['pass_word']==$password)
     		{
-    			echo "welcome";
+    			
+                $_SESSION['firstname']=$row['first_name'];
+                $_SESSION['lastname']=$row['last_name'];
+                $_SESSION['email']=$row['user_email'];
+                $_SESSION['gender']=$row['user_gender'];
+                $_SESSION['hometown']=$row['home_town'];
+                $_SESSION['aboutme']=$row['about_me'];
+                $_SESSION['status']=$row['user_status'];
+                $_SESSION['phone']=$row['phone_number'];
+                $_SESSION['bday']=$row['birth_date'];
+              
+                
     			header('Location: ../profile.html');
     		}
     		else
--- PHP/signup.php
+++ PHP/signup.php
@@ -1,8 +1,9 @@
 <?php
-$servername = "127.0.0.1";
+$servername = "localhost";
 $username = "root";
 $password = "";
 $db="socialnetwork";
+session_start();
 
 // Create connection
 $conn = new mysqli($servername, $username, $password,$db);
@@ -14,51 +15,58 @@ if ($conn->connect_error)
     } 
     else
     {
+    	
+
 		if (isset($_POST['reg']))
 		{
-			$firstname=$_POST['firstName'];
-			echo $firstname;
-			$lastname=$_POST['lastName'];
-			echo $lastname;
-			$nickname=$_POST['nickName'];
-			echo $nickname;
-			$password=$_POST['Password'];
-			echo $password;
-			$email=$_POST['Email'];
-			echo $email;
-			$phone=$_POST['phoneNumber'];
-			echo $phone;
-			$hometown=$_POST['homeTown'];
-			echo $hometown;
-			$aboutme=$_POST['me'];
-			echo $aboutme;
-			$mstatus=$_POST['status'];
-			echo $mstatus;
-			$birthdate=$_POST['bday'];
-			echo $birthdate;
-			$gender=$_POST['sex'];
-			echo $gender;
-			$frnds = 10;
 
-			if($_FILES["pic"]["error"] > 0){
-				if($gender == "Male"){
-					$img = "./img/male_default.jpg";
-				}
-				else{
-				$img = "./img/female_default.jpg";
-				}
-			} else{
-				$img = $_FILES["pic"];
-				echo "File Name: " . $_FILES["pic"]["name"] . "<br>";
-				echo "File Type: " . $_FILES["pic"]["type"] . "<br>";
-				echo "File Size: " . ($_FILES["pic"]["size"] / 1024) . " KB<br>";
-				echo "Stored in: " . $_FILES["pic"]["tmp_name"];
-			}
-			echo $img;
-			$sql = "INSERT INTO user_(first_name, last_name, nick_name, pass_word, user_email, phone_number, home_town, about_me, user_status, birth_date, user_gender, number_friends, user_image)VALUES('$firstname','$lastname','$nickname','$password','$email','$phone','$hometown','$aboutme','$mstatus','$birthdate','$gender', '$frnds', '$img')";
+         
+			$firstname=$_POST['firstName'];
+			$lastname=$_POST['lastName'];
+			$nickname=$_POST['nickName'];
+			$password=$_POST['Password'];
+			$email=$_POST['Email'];
+		    $phone=$_POST['phoneNumber'];
+			$aboutme=$_POST['me'];
+			$mstatus=$_POST['status'];
+			$birthdate=$_POST['bday'];
+			$gender=$_POST['sex'];
+			$hometown=$_POST['homeTown'];
+
+			$friends = 10;
+				$_SESSION['firstname']=$firstname;
+				$_SESSION['lastname']=$lastname;
+				$_SESSION['email']=$email;
+				$_SESSION['gender']=$gender;
+				$_SESSION['hometown']=$hometown;
+				$_SESSION['aboutme']=$aboutme;
+				$_SESSION['status']=$mstatus;
+				$_SESSION['phone']=$phone;
+				$_SESSION['bday']=$birthdate;
+
+
+   //              if($_FILES['pic']['error'] > 0){
+			// 	if($gender == "Male"){
+			// 		$img = "./img/male_default.jpg";
+			// 	}
+			// 	else{
+			// 	$img = "./img/female_default.jpg";
+			// 	}
+			// } else{
+			// 	$img = $_FILES["pic"];
+			// 	echo "File Name: " . $_FILES["pic"]["name"] . "<br>";
+			// 	echo "File Type: " . $_FILES["pic"]["type"] . "<br>";
+			// 	echo "File Size: " . ($_FILES["pic"]["size"] / 1024) . " KB<br>";
+			// 	echo "Stored in: " . $_FILES["pic"]["tmp_name"];
+			// }
+			// echo $img;
+			$sql = "INSERT INTO user_(first_name, last_name, nick_name, pass_word, user_email, phone_number, home_town, about_me, user_status, birth_date, user_gender, number_friends, user_image)VALUES('$firstname','$lastname','$nickname','$password','$email','$phone','$hometown','$aboutme','$mstatus','$birthdate','$gender', '$frnds', '')";
 			$qry = mysqli_query($conn,$sql);
+
 			if($qry){
 				echo "done";
+				header("location: ../profile.html");
+
 			}
 
 
@@ -70,3 +78,5 @@ if ($conn->connect_error)
 }
 
 ?>
+
+
