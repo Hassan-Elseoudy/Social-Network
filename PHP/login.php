e210bf743cea91ea60db67f426a033d311183f19
MernaAdel <31679900+mernaadell@users.noreply.github.com>
2019-05-18 18:33:57 -0700

    Add files via upload

--- PHP/login.php
+++ PHP/login.php
@@ -3,7 +3,7 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $db="socialnetwork";
-
+$output="";
 // Create connection
 $conn = new mysqli($servername, $username, $password,$db);
 
@@ -18,20 +18,26 @@ if ($conn->connect_error)
 		if (isset($_POST['login']))
 		{
 			
-			$password=$_POST['Password'];
+			$password=$_POST['password'];
 			$email=$_POST['email'];
-			$sql="Select FROM user(`first_name`, `last_name`, `nick_name`, `pass_word`, `user_email`, `phone_number`, `home_town`, `about_me`, `user_status`, `birth_date`, `user_gender`, `user_image`)VALUES('$firstname','$lastname','$nickname','$password','$email','$phone','$hometown','$aboutme','$mstatus','$birtdate','$gender','$')";
-			$qry=mysqli_query($conn,$sql);
-			if($qry){
-				echo "done";
-			$select=$db->prepare("SELECT * FROM user WHERE user_email='$email' and pass_word='$password'");
-			$select->setFetchMode(PDO::FETCH_ASSOC);
-			$select->execute();
-			$data=$select->fetch();
 			
-			$_SESSION ['email']=$data['user_email'];
-			$_SESSION ['password']=$data['pass_word'];
-			}
+    		$q=mysqli_query($conn,"SELECT * FROM user WHERE user_email='$email' and pass_word='$password'");
+    		$row=mysqli_fetch_array($q);
+    		if($row['user_email']==$email&&$row['pass_word']==$password)
+    		{
+    			echo "welcome";
+    			header('Location: ../profile.html');
+    		}
+    		else
+    		{
+    			echo "invalid";
+    		}
+
+    		
+    				
+    			
+			
+			
 
 
 		}
