97697c3e03b3c90f2c10628abb7dcaaba58b8aea
MernaAdel <31679900+mernaadell@users.noreply.github.com>
2019-05-19 09:29:29 -0700

    Add files via upload

---
+++ PHP/user.php
@@ -0,0 +1,36 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db="socialnetwork";
+session_start();
+$output=" Results  -> ";
+// Create connection
+$conn = new mysqli($servername, $username, $password,$db);
+
+// Check connection
+if ($conn->connect_error)
+	{
+    	die("Connection failed: " . mysqli_connect_error());
+    } 
+    else
+        {  
+            
+             echo $_SESSION['friendfirstname'].' <br'
+                        .$_SESSION['friendlastname'].'<br> '
+                        .$_SESSION['friendemail'].' <br>'
+                        .$_SESSION['friendgender'].'<br> '
+                        .$_SESSION['friendhometown'].' <br>'
+                        .$_SESSION['friendstatus'].'<br> '
+                        .$_SESSION['friendphone'].'<br> '
+                        ;  
+           
+
+	
+			
+
+   $conn->close();
+}
+
+?>
